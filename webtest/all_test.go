d082ba098a14d7a9fd1aa9e39b1c50e44e8cf908
Hiromu OCHIAI <otiai10@gmail.com>
2021-09-02 10:08:10 +0900

    Add webtest

---
+++ webtest/all_test.go
@@ -0,0 +1,24 @@
+package webtest
+
+import (
+	"testing"
+
+	"github.com/otiai10/gh-dependents/ghdeps"
+	. "github.com/otiai10/mint"
+)
+
+func TestCrawler_All(t *testing.T) {
+	c := ghdeps.NewCrawler("cli/cli")
+	err := c.All()
+	Expect(t, err).ToBe(nil)
+	Expect(t, len(c.Pages) > 0).ToBe(true)
+	Expect(t, len(c.Dependents) >= 20).ToBe(true)
+}
+
+func TestCrawler_Crawl(t *testing.T) {
+	c := ghdeps.NewCrawler("otiai10/lookpath")
+	err := c.Crawl(1)
+	Expect(t, err).ToBe(nil)
+	Expect(t, len(c.Pages)).ToBe(1)
+	Expect(t, len(c.Dependents)).ToBe(30)
+}
